e7a78855b212f8fe4bef7c6a2fc862d46871877c
davisPB <vladpb27>
2023-06-14 13:11:43 -0500

    vire.config updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/puppies-app/'
+  base: 'https://vladPB27.github.io/puppies-app/'
 })
